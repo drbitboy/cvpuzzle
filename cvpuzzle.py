494413dd3ef41c6ab06825153a6b1e6e44d55977
Brian Carcich <briantcarcich@gmail.com>
2020-03-16 20:30:03 -0400

    Add comments to cvpuzzle.py

--- cvpuzzle.py
+++ cvpuzzle.py
@@ -1,6 +1,14 @@
+"""
+Usage:  make all ; python cvpuzzle.py
+
+Pre-requisite:  pyfb1002.py, via command [make all]
+
+"""
 import pyfb1002
 
 x = .11011
 N = 10000
-print('fb1002({0},{1}) => {2}'.format(
-       x,N,pyfb1002.orig_fb1002(x,N)))
+print('fb1002({0},{1}) => {2}'.format(x
+                                     ,N
+                                     ,pyfb1002.orig_fb1002(x,N)
+                                     ))
