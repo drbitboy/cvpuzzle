22d3823fca64aab976d8359822d649b255f94b34
Brian Carcich <briantcarcich@gmail.com>
2020-03-16 20:10:16 -0400

    Add comments to alternate implementation btcfb1002.py; simplify code also

--- btcfb1002.py
+++ btcfb1002.py
@@ -1,4 +1,13 @@
 """
+btcfb1002.py
+
+Alternate implementation of algorithm from [fb1002.txt], using Python 
+- Method .btc_fb1002
+- See test_cvpuzzle.py for usage
+- Also Uses locally-scoped variables lcl_* for numerator and denominator
+  that are updated on each pass through the while loop
+
+
 FUNCTION_BLOCK "fb1002"
 { S7_Optimized_Access = 'TRUE' }
 VERSION : 0.1
@@ -25,40 +34,54 @@ VERSION : 0.1
 
 BEGIN
 """
-import os
-import sys
 DINT_TO_LREAL = DINT_TO_REAL = float
 def btc_fb1002(x,N):
-        diVar1 = 0
-        diVar2 = 1
-        diVar3 = 1
-        diVar4 = 1
+
+        ### Initialize integer parameters
+        diVar1 = 0  ### Numerator part I
+        diVar2 = 1  ### Denominator part I
+        diVar3 = 1  ### Numerator part II
+        diVar4 = 1  ### Denominator part II
+
+        ### Continue loop while denominator parameters are less than N
         while diVar2 <= N and diVar4 <= N:
+
+                    ### Calculate combined numerator and combined denominator
                     lcl_numer  = diVar1 + diVar3
                     lcl_denom  = diVar2 + diVar4
+
+                    ### Calculate floating-point ratio
                     lrVar1 = DINT_TO_LREAL(lcl_numer) / DINT_TO_REAL(lcl_denom)
+
+                    ### If ratio matches target [x] ...
                     if x == lrVar1:
+
+                        ### Choose which numerator and denominator pair to use
                         if lcl_denom < N:
                             Numerator = lcl_numer
                             Denominator = lcl_denom
-                            return Numerator,Denominator
-                            
                         elif diVar4 > diVar2:
                             Numerator = diVar3
                             Denominator = diVar4
-                            return Numerator,Denominator
                         else:
                             Numerator = diVar1
                             Denominator = diVar2
-                            return Numerator,Denominator
 
-                    elif x > lrVar1:
+                        ### Exit loop early as well as method
+                        return Numerator,Denominator
+
+                    ### Otherwise, adjuts either ...
+                    if x > lrVar1:
+                        ### ... part I parameters to increase ratio, ...
                         diVar1 = lcl_numer
                         diVar2 = lcl_denom
                     else:
+                        ### ... or part II parameters to decrease ratio
                         diVar3 = lcl_numer
                         diVar4 = lcl_denom
 
+        ### On loop exit* use best parts for result
+        ### * Exact ratio not found; diVar2 or diVar4 exceeded N
         if diVar2 > N:
             Numerator = diVar3
             Denominator = diVar4
@@ -66,4 +89,5 @@ def btc_fb1002(x,N):
             Numerator = diVar1
             Denominator = diVar2
 
+        ### Exit method
         return Numerator,Denominator
